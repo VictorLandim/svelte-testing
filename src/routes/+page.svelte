ed230a4f87ca71ee97c1291025ecec332b670020
Victor Landim <victorlandim5@gmail.com>
2026-03-19 21:53:58 +0100

    update again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <h2>This is an update</h2>
 <p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation dasdasda
+    dasdsa Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to
+    read the documentation dasdasda
 </p>
